a29acf1c0b33f5883d0354771b81f9949545b070
Shayna Symons <shaynas@shopvisible.com>
2012-10-17 01:19:35 -0400

    Git displeases me.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 jqueryui-demo-sample
 ====================
 
-jQuery UI Demo Overhaul
+<h1>jQuery UI Composite Demo Sample</h1>
+<p>Composite demos showing how to use multiple jQuery UI Widgets in real-world scenarios.</p>
+<p><a href="http://shaynasymons.github.com/jqueryui-demo-sample/compositeDemos.html">Demo Page</a></p>
--- js/compositeDemos.js
+++ js/compositeDemos.js
@@ -1,42 +1,51 @@
 jQuery.noConflict();
 jQuery.fn.exists = function () { return this.length > 0; }
 jQuery(document).ready(function ($) {
-    //Demos Nav
-    /*$(".entry-listing-parent").live("click", function () {
-        $(".entry-listing-children:visible").each(function () {
-            $(this).toggle("blind", 500);
+    /* Common Functionality */
+    //Numeric Check
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+    //Watermark 
+    $(".watermark").each(function () {
+        if ($(this).val() == "") {
+            $(this).val(jQuery(this).attr("title")).addClass("watermarked");
+        }
+        $(this).focus(function () {
+            if (jQuery(this).val() == jQuery(this).attr("title")) {
+                jQuery(this).val("").removeClass("watermarked");
+            }
         });
-        $(this).parent().children(".entry-listing-children").toggle("blind", 500);
-    });*/
+        $(this).blur(function () {
+            if ($(this).val() == "") {
+                $(this).val($(this).attr("title")).addClass("watermarked");
+            }
+        });
+    });
+    //Accordion
+    $(".entry-inner-bottom-info").accordion({
+        collapsible: true,
+        heightStyle: "content"
+    });
+    //Tabs
+    $(".tabs-outer-wrap").tabs();
+    //Tool Tips
+    $(".bottom-content-widgets li .info-icon-wrap").tooltip({
+        items: "[title]",
+        content: function () {
+            return $(this).attr("title");
+        }
+    });
+
+    /* Demos Index Functionality */
+    //Demos Right Nav
     $(".entry-listing-link").live("click", function () {
         var newDemo = $(this).attr("rel");
         $(".demo-frame").attr("src", newDemo);
     });
     $(".entry-listing-nav").accordion({
         collapsible: true,
+        heightStyle: "content",
         active: false
-    });
-    //General Functions
-    jQuery('.watermark').each(function () {
-        if (jQuery(this).val() == "") {
-            jQuery(this).val(jQuery(this).attr('title')).addClass('watermarked');
-        }
-        jQuery(this).focus(function () {
-            if (jQuery(this).val() == jQuery(this).attr('title')) {
-                jQuery(this).val("").removeClass('watermarked');
-            }
-        });
-        jQuery(this).blur(function () {
-            if (jQuery(this).val() == "") {
-                jQuery(this).val(jQuery(this).attr('title')).addClass('watermarked');
-            }
-        });
-    });
-    //Tool Tips
-    $(".entry-content-desc li .info-icon-wrap").tooltip({
-        items: "[title]",
-        content: function () {
-            return $(this).attr("title");
-        }
     });
 });
